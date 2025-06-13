491783de9597e5605b9df6e29970701cecb28014
kim kwan yong <slow33@gmail.com>
2025-06-14 08:52:13 +0900

    add changes

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <img src={`md:hero-desktop.png hero-mobile.png`}></img>
         </div>
 
       </div>
